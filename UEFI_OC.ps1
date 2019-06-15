cb04c08b1b598b9c8c98fde8d81c83c96763074a
CodesBlue <31323907+codesblue@users.noreply.github.com>
2019-06-15 08:29:35 -0300

    UEFI USB CD
    Con este pequeño script podrás  crear una usb booteable con formato UEFI sin necesidad de utilizar programas de terceros.

---
+++ UEFI_OC.ps1
@@ -0,0 +1,58 @@
+﻿$iso = Read-Host "Pega la ruta de tu iso"
+
+$formato = Read-Host "Tipo de particion (GPT O MBR)"
+
+$fat = Read-Host "Tipo de formato (FAT32 O NTFS)"
+
+$extension = Read-Host "Por favor introduzca  el tipo de extension  ej: .win o .esd"
+
+# Clean ! will clear any plugged-in USB stick!!
+Get-Disk | Where BusType -eq 'USB' | Clear-Disk -RemoveData -Confirm:$true -PassThru
+
+# Convert GPT
+if ((Get-Disk | Where BusType -eq 'USB').PartitionStyle -eq 'RAW') {
+    Get-Disk | Where BusType -eq 'USB' | 
+    Initialize-Disk -PartitionStyle $formato
+} else {
+    Get-Disk | Where BusType -eq 'USB' | 
+    Set-Disk -PartitionStyle $formato
+}
+
+# Create partition primary and format to FAT32
+$volume = Get-Disk | Where BusType -eq 'USB' | 
+New-Partition -UseMaximumSize -AssignDriveLetter | 
+Format-Volume -FileSystem $fat
+
+if (Test-Path -Path "$($volume.DriveLetter):") {
+
+    # Mount iso
+    $miso = Mount-DiskImage -ImagePath $iso -StorageType ISO -PassThru
+
+    # Driver letter
+    $dl = ($miso | Get-Volume).DriveLetter
+}
+
+if (Test-Path -Path "$($dl):\sources\install.$extension") {
+
+    # Copy ISO content to USB except install.wim
+    & (Get-Command "$($env:systemroot)\system32\robocopy.exe") @(
+        "$($dl):",
+        "$($volume.DriveLetter):"
+        ,'/S','/R:0','/Z','/XF','install.$extencion','/NP'
+    )
+
+    # Split install.wim
+    & (Get-Command "$($env:systemroot)\system32\dism.exe") @(
+        '/split-image',
+        "/imagefile:$($dl):\sources\install.$extension",
+        "/SWMFile:$($volume.DriveLetter):\sources\install.swm",
+        '/FileSize:4096'
+    )
+}
+
+# Eject USB
+(New-Object -comObject Shell.Application).NameSpace(17).
+ParseName("$($volume.DriveLetter):").InvokeVerb('Eject')
+
+# Dismount ISO
+Dismount-DiskImage -ImagePath $iso
